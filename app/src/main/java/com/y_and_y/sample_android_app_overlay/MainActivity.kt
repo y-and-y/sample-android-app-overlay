9f6d19e13fce1492bfbf855625bcd4043bf69244
kkt-yu <ku.k669500@gmail.com>
2018-12-27 22:14:00 +0900

    :tada: initial commit

---
+++ app/src/main/java/com/y_and_y/sample_android_app_overlay/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.y_and_y.sample_android_app_overlay
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
